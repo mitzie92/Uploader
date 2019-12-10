541da8ff64408b19cb29f2683cb7a6742f6af139
Aaron Ballard <ans.ballard@gmail.com>
2019-12-09 20:53:37 -0500

    WIP 2.0
    - added local.ts methods to refactor for storing multiple profiles

--- src/frontend/store/local.ts
+++ src/frontend/store/local.ts
@@ -1,5 +1,35 @@
 import { User } from "@modwatch/types";
 
+export const getUsers = (): User[] => {
+  const users = localStorage.getItem("modwatch.users");
+  if(!users) {
+    return [];
+  }
+  try {
+    return JSON.parse(users);
+  } catch (e) {
+    console.log(`Failed to parse saved users: "${users}"`, e);
+    return clearUsers();
+  }
+}
+
+export const setCurrentUser = (username: string): User => {
+  const users = getUsers();
+  const user = users.find(user => user.username === username);
+  if(!user) {
+    throw new Error("Cannot set current user if no local matching username is found");
+  }
+  return localStorage.setItem("modwatch.currentuser", JSON.stringify(username)), user
+}
+
+export const setUsers = (users: User[]): User[] => (
+  localStorage.setItem("modwatch.users", JSON.stringify(users)), users
+)
+
+export const clearUsers = (): [] => (
+  localStorage.setItem("modwatch.users", "[]"), []
+);
+
 export const getUserState = (): User | {} => {
   const user = localStorage.getItem("modwatch.user");
   if (!user) {
